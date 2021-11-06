3e1e9e331db258efbcb3165b0266b5a29442c39e
Zimin7941VC <zimin008@rambler.ru>
2021-11-06 23:45:49 +0300

    add functions in Keeper

--- laba_05_01_08/Keeper.cpp
+++ laba_05_01_08/Keeper.cpp
@@ -1,23 +1,94 @@
 #include "Keeper.h"
 
+Keeper::Keeper()
+{
+	std::cout << "Keeper()\n";
+}
+
+Keeper::~Keeper()
+{
+	std::cout << "~Keeper()\n";
+}
+
 void Keeper::print()
 {
+	for (size_t i = 0; i < shops.size(); i++)
+		std::cout << "Книжный магазин №" << i + 1 << ":\n" << shops[i]->getPrintData();
 }
 
 void Keeper::add(Shop* elem)
 {
+	shops.push_back(elem);
 }
 
 void Keeper::del(size_t num)
 {
+	delete shops.pop(num);
 }
 
-bool Keeper::save(std::string file)
+bool Keeper::save(string file)
 {
-	return false;
+	std::ofstream fout(file);
+	if (!fout) return 0;
+	fout << to_string(shops.size()) << std::endl;
+	for (size_t i = 0; i < shops.size(); ++i)
+		fout << shops[i]->getData();
+
+	fout.close();
+	return 1;
 }
 
-bool Keeper::load(std::string file)
+bool Keeper::load(string file)
 {
-	return false;
+	std::ifstream fin(file);
+	if (!fin) return 0;
+
+	std::string type;
+	size_t shop_amount;
+	double cost;
+
+	fin >> shop_amount;
+	for (size_t i = 0; i < shop_amount; i++)
+	{
+		fin >> type;
+		fin >> cost;
+		if (type == "Book")
+		{
+			std::string title, author, annotation, genre;
+			int year_of_issue, page_volume;
+			getline(fin, title);
+			getline(fin, author);
+			getline(fin, annotation);
+			getline(fin, genre);
+			fin >> year_of_issue;
+			fin >> page_volume;
+			shops.push_back(new Book(cost, title, author, annotation, genre, year_of_issue, page_volume));
+		}
+		else if (type == "Textbook")
+		{
+			std::string title, author, institution;
+			int year_of_issue, page_volume, year_of_study;
+			getline(fin, title);
+			getline(fin, author);
+			getline(fin, institution);
+			fin >> year_of_issue;
+			fin >> page_volume;
+			fin >> year_of_study;
+			shops.push_back(new Textbook(cost, title, author, institution, year_of_issue, page_volume, year_of_study));
+		}
+		else if (type == "Stationery")
+		{
+			std::string type, color, purpose;
+			getline(fin, type);
+			getline(fin, color);
+			getline(fin, purpose);
+			shops.push_back(new Stationery(cost, type, color, purpose));
+		}
+	}
+	return 1;
+}
+
+size_t Keeper::size()
+{
+	return shops.size();
 }
--- laba_05_01_08/Keeper.h
+++ laba_05_01_08/Keeper.h
@@ -1,18 +1,24 @@
 #pragma once
-#include "List.h"
-#include "Shop.h"
 #include <fstream>
 #include <string>
+#include "List.h"
+#include "Shop.h"
+#include "Book.h"
+#include "Textbook.h"
+#include "Stationery.h"
 class Keeper
 {
 private:
-	List<Shop*> garages;
+	List<Shop*> shops;
 
 public:
+	Keeper();
+	~Keeper();
 	void print();					// вывод на экран производных объектов абстрактного класса Base
 	void add(Shop* elem);	// добавление     производных объектов абстрактного класса Base
 	void del(size_t num);			// удаление       производных объектов абстрактного класса Base
 	bool save(std::string file);			// полное сохранение     себя в  файле
 	bool load(std::string file);			// полное восстановление себя из файла
+	size_t size();
 };
 
